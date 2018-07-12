65d9a4a71210092ff90f5ef06e4e12e6c8b77d1c
Ella Sklar <ella.soccer22@gmail.com>
2018-07-12 13:52:57 +0000

    automatically backed up by learn

--- sc.rb
+++ sc.rb
@@ -1,18 +1,18 @@
+# Level 1
+# states = {"Massachusetts" => "MA",
+#           "Wisconsin" => "WI",
+#           "New Jersey" => "NJ",
+#           "Colorado" => "CO", 
+#           "Maine"=> "ME"}
 
-states = {"Massachusetts" => "MA",
-          "Wisconsin" => "WI",
-          "New Jersey" => "NJ",
-          "Colorado" => "CO", 
-          "Maine"=> "ME"}
-
-capitals = {"MA" => "Boston",
-            "WI" => "Madison",
-			      "NJ" => "Trenton",
-	 		      "CO" => "Denver", 
-	 		      "ME" => "Augusta"}
+# capitals = {"MA" => "Boston",
+#             "WI" => "Madison",
+# 			      "NJ" => "Trenton",
+# 	 		      "CO" => "Denver", 
+# 	 		      "ME" => "Augusta"}
 	 		      
-given = "Colorado"
-puts capitals[states[given]]
+# given = "Colorado"
+# puts capitals[states[given]]
 
 
 # Level 2
@@ -29,4 +29,10 @@ capitals = {"MA" => "Boston",
 			      "NJ" => "Trenton",
 	 		      "CO" => "Denver", 
 	 		      "ME" => "Augusta"}
+	 		      
+	 		      given = "North Alaska"
+	 		      if capitals[states[given]].nil?
+	 		        puts "Unknown"
+	 		      else 
+	 		        puts 
 
