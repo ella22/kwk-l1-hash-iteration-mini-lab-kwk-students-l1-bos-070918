21139497c6ad61eb4311fce8bdf653a15a0b3bb7
Ella Sklar <ella.soccer22@gmail.com>
2018-07-12 14:09:58 +0000

    automatically backed up by learn

--- one_direction.rb
+++ one_direction.rb
@@ -1,10 +1,9 @@
+                {"Zayn" => 25,
+                 "Liam" => 24,
+                 "Louis" => 26,
+                 "Harry" => 24,
+                 "Niall" => 24 
+}
 
- one_direction.each do |x|
-   puts x 
-   
-   puts "Which one direction memebr would you like to know the age of?"
-   given = gets.chomp
-    
-    puts one_direction[given]
-    
- end 
+ one_direction.each do |name,age|
+   puts "#{name} is #{age} years old. "
