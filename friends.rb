1984d0e52c7d8ee35906d38e9e05a3f08a21d96b
Ella Sklar <ella.soccer22@gmail.com>
2018-07-12 14:25:50 +0000

    automatically backed up by learn

---
+++ friends.rb
@@ -0,0 +1,11 @@
+character_list = { 
+  "Monica" => "Neat Freak",
+  "Joey" => "Jokester",
+  "Chandler" => "Saracastic n00b", 
+  "Pheobe" => "Free Spirit",
+  "Rachel" => "Shopaholic",
+  "Ross" => "Divorcee",
+
+}
+
+character_list.each do |character_name, character_trait|
