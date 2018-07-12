af2d592d5e009b29a7e1daf2259856c5ed1bddef
Ella Sklar <ella.soccer22@gmail.com>
2018-07-12 14:15:03 +0000

    automatically backed up by learn

--- one_direction.rb
+++ one_direction.rb
@@ -5,6 +5,7 @@
                  "Niall" => 24 
 }
 
- one_direction.each do |name,age|
-   puts "#{name} is #{age} years old."
- end 
+sum = 0 
+one_direction.each do |name, age|
+  sum += age
+end 
