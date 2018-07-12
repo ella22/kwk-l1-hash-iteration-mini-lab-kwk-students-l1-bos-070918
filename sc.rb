89ad9a3c71a3f99c034ac13d7f172387bb323caf
Ella Sklar <ella.soccer22@gmail.com>
2018-07-12 13:51:46 +0000

    automatically backed up by learn

--- sc.rb
+++ sc.rb
@@ -15,5 +15,18 @@ given = "Colorado"
 puts capitals[states[given]]
 
 
+# Level 2
 
+
+states = {"Massachusetts" => "MA",
+          "Wisconsin" => "WI",
+          "New Jersey" => "NJ",
+          "Colorado" => "CO", 
+          "Maine"=> "ME"}
+
+capitals = {"MA" => "Boston",
+            "WI" => "Madison",
+			      "NJ" => "Trenton",
+	 		      "CO" => "Denver", 
+	 		      "ME" => "Augusta"}
 
